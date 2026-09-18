39b7e46c11ae3056b051f3170d6be29f3ff2e0a6
김민곤 <minggong222@naver.com>
2026-09-18 22:09:28 +0900

    Content Browser

--- Week4/SceneManager.h
+++ Week4/SceneManager.h
@@ -8,6 +8,7 @@
 #include "enum.h"
 #include "FAssetManager.h"
 #include "PropertyPanel.h"
+#include <string>
 
 inline constexpr std::string_view kSceneDataDir = "SceneData\\";
 inline constexpr std::string_view kSceneDataSuffix = ".Scene";
@@ -97,8 +98,15 @@ private:
 	FPropertyPanel* mPropertyPanel = nullptr;
 
 	//Content Browser
-
-
+	std::filesystem::path mRootPath = "Assets";
+	std::filesystem::path mCurrentDirectory = "Assets";
+	std::filesystem::path mSelectedAssetPath = "";
+	char mSearchBuffer[256] = "";
+	float mThumbnailSize = 64.0f;
+	bool mShowContentBrowser = false;
+	void updateContentBrowserGUI(const FGuiReference& guiReference);
+	void drawFolderTree(const std::filesystem::path& currentPath);
+	void drawAssetGrid();
 
 	void updateControlPanelGUI(const FGuiReference& guiReference);
 
@@ -106,5 +114,4 @@ private:
 	//지금은 하드코딩
 	void updatePropertyWindowGUI(const FGuiReference& guiReference);
 	void updateOutlinerGUI(const FGuiReference& guiReference);
-	void updateContentBrowserGUI(const FGuiReference& guiReference);
 };
